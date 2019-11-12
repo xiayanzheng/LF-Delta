bf55ec5c37855776519f3366ba21884055e1bcc4
LF <xiayanzheng@outlook.com>
2019-11-12 19:48:20 +0800

    report merge function now available in report_handel

--- handel_report/mergeReports.py
+++ handel_report/mergeReports.py
@@ -7,10 +7,25 @@ from xlutils.copy import copy as xl_copy
 # Load Report
 report_root = DaPr.find_path_backward(os.getcwd(), 'Reports')
 report_objs = os.listdir(report_root)
-report_obj_dev = os.path.join(report_root, report_objs[0])
-report_obj_dev_objs = os.listdir(report_obj_dev)
+report_objs_dev = os.path.join(report_root, report_objs[0])
+report_objs_dev_objs = os.listdir(report_objs_dev)
+merged = pd.ExcelWriter(os.path.join(report_objs_dev, "merged.xlsx"))
+dfs = []
 
+for x in report_objs_dev_objs:
+    report_objs_dev_sub = os.path.join(report_objs_dev, x)
+    for obj in os.listdir(report_objs_dev_sub):
+        if os.path.splitext(obj)[-1] == '.csv' and "Summary" in obj:
+            csv_file = os.path.join(report_objs_dev_sub, obj)
+            csv_data = pd.read_csv(csv_file)
+            csv_data.set_index('item', inplace=True)
+            dfs.append(csv_data)
 
-
-
-os.system("explorer %s" % os.getcwd())
+final = dfs[0]
+print(type(final))
+for df in dfs[1:]:
+    print(type(df))
+    temp = final.join(df, how='outer', lsuffix='item')
+    final = temp
+print(final)
+final.to_excel("mgs.xlsx", engine='xlsxwriter')
